29cc5a9441e14f4e3eeb57a486fe4363901401e6
Alex-kornel <73165954+Alex-kornel@users.noreply.github.com>
2024-11-17 14:37:15 +0200

    Update import_geojson.py

--- geomap/src/import_geojson.py
+++ geomap/src/import_geojson.py
@@ -4,8 +4,8 @@ import mysql.connector
 def connect_to_db():
     return mysql.connector.connect(
         host="localhost",
-        user="root",
-        password="Mybdpas_1",  
+        user="",
+        password="",  
         database="ukraine_map"
     )
 
